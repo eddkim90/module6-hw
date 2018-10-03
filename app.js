8afc9de5340141b2e43a0d44498ae58a039c9235
Edward Kim <edprodigy07@gmail.com>
2018-10-03 17:57:05 -0400

    Renamed the App Name

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    angular.module('Assignment1', []).controller('LunchCheckController', LunchCheckController);
+    angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
 
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope) {
